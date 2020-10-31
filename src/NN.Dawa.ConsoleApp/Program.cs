7f1e6727d69afbe5ba06808bfd0e46ae774952b0
william <wiw@elefantnet.dk>
2020-10-31 12:36:34 +0100

    Initial commit

---
+++ src/NN.Dawa.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mock.Dawa.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
