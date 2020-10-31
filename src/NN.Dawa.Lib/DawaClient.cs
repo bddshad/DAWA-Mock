167a404075df14ca40c5ba4d22bdfaebfc02a381
bddshad <70568931+bddshad@users.noreply.github.com>
2020-10-31 12:43:55 +0100

    Add scaffolding for DAWA Client

---
+++ src/NN.Dawa.Lib/DawaClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mock.Dawa.Lib
+{
+    public class DawaClient
+    {
+        private string _host;
+
+        public DawaClient(string host)
+        {
+            _host = host;
+        }
+
+        public string GetDataFromDawa(string query)
+        {
+            return "YES";
+        }
+    }
+}
