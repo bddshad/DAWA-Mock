b10e331ecb3858bea5b4d6b8f2ba27604160d1aa
bddshad <70568931+bddshad@users.noreply.github.com>
2020-10-31 15:00:48 +0100

    the code will work ✌

--- src/NN.Dawa.ConsoleApp/Program.cs
+++ src/NN.Dawa.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mock.Dawa.Lib;
+using System;
+using System.IO;
 
 namespace Mock.Dawa.ConsoleApp
 {
@@ -7,6 +9,31 @@ namespace Mock.Dawa.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var client = new DawaClient();
+
+            for (int i = 0; i < 300; i++)
+            {
+                var recievedData = client.GetDataFromDawa(GetRandomString());
+                Console.WriteLine(recievedData);
+                StreamWriter writer = new StreamWriter("../../../output.csv", true);
+                writer.WriteLine(recievedData);
+                writer.Close();
+            }
+
+        }
+        static string GetRandomString()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+            var stringChars = new char[4];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            return finalString;
         }
     }
 }
--- src/NN.Dawa.Lib/DawaClient.cs
+++ src/NN.Dawa.Lib/DawaClient.cs
@@ -1,19 +1,53 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Net.WebSockets;
 
 namespace Mock.Dawa.Lib
 {
     public class DawaClient
     {
-        private string _host;
 
-        public DawaClient(string host)
+        private static HttpClient _client;
+
+        public DawaClient()
         {
-            _host = host;
+
+            _client = new HttpClient();
+
+            _client.BaseAddress = new Uri("https://dawa.aws.dk/");
+
         }
 
         public string GetDataFromDawa(string query)
         {
-            return "YES";
+
+            var url = $"/autocomplete?q={query}&fuzzy=true";
+            var response = _client.GetStringAsync(url).Result;
+            var jarray= JArray.Parse(response);
+            var vejnavn = jarray.First["forslagstekst"].ToString();
+
+            url = $"/autocomplete?q={vejnavn}%20&fuzzy=true";
+            response = _client.GetStringAsync(url).Result;
+            jarray = JArray.Parse(response);
+            if (jarray.First["type"].ToString() == "vejnavn")
+            {
+                var random = new Random();
+                var randomised = random.Next(0, 6);
+                url = $"/autocomplete?q={vejnavn}%20{randomised}&fuzzy=true";
+                response = _client.GetStringAsync(url).Result;
+                jarray = JArray.Parse(response);
+                var addresse = $"{jarray.First["forslagstekst"]};{jarray.First["data"]["postnr"]};{jarray.First["data"]["postnrnavn"]};{jarray.First["data"]["href"]};{jarray.First["data"]["id"]}";
+
+                return addresse;
+            }
+            else
+            {
+                var addresse = $"{jarray.First["forslagstekst"]};{jarray.First["data"]["postnr"]};{jarray.First["data"]["postnrnavn"]};{jarray.First["data"]["href"]};{jarray.First["data"]["id"]}";
+
+                return addresse;
+            }
+
         }
     }
 }
